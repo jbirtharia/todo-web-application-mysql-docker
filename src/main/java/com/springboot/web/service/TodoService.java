e6889f9584c26ec7c7276bb632e5d77297dac3d8
nitinParkway <47175248+nitinParkway@users.noreply.github.com>
2020-07-20 16:02:43 +0530

    Commit changes

--- src/main/java/com/springboot/web/service/TodoService.java
+++ src/main/java/com/springboot/web/service/TodoService.java
@@ -14,10 +14,10 @@ public class TodoService {
     private static int todoCount = 3;
 
     static {
-        todos.add(new Todo(1, "in28minutes", "Learn Spring MVC", new Date(),
+        todos.add(new Todo(1, "jayendra", "Learn Spring MVC", new Date(),
                 false));
-        todos.add(new Todo(2, "in28minutes", "Learn Struts", new Date(), false));
-        todos.add(new Todo(3, "in28minutes", "Learn Hibernate", new Date(),
+        todos.add(new Todo(2, "jayendra", "Learn Struts", new Date(), false));
+        todos.add(new Todo(3, "jayendra", "Learn Hibernate", new Date(),
                 false));
     }
 
